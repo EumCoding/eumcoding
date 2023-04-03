ec0c527c274280a5afdebfcdcf7b37c42afc6494
lv2dev <lv2dev@gmail.com>
2023-04-03 15:13:01 +0900

    first commit

---
+++ src/main/java/com/latteis/eumcoding/EumcodingApplication.java
@@ -0,0 +1,13 @@
+package com.latteis.eumcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumcodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumcodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/latteis/eumcoding/EumcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latteis.eumcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumcodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
