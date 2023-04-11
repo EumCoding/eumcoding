3571f25a7ba6032a37895fb5c98771eb141d6520
lsh <saunghunlee@naver.com>
2023-04-11 14:00:09 +0900

    기본적인 로그읜,회원가입기능

---
+++ src/main/java/com/latteis/eumcoding/controller/EmailController.java
@@ -0,0 +1,35 @@
+package com.latteis.eumcoding.controller;
+
+import com.latteis.eumcoding.dto.ResponseDTO;
+import com.latteis.eumcoding.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/member")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @GetMapping("/confirm-email")
+    public ResponseEntity<?> viewConfirmEmail(@RequestParam String token){
+        try {
+            boolean result = emailService.verifyEmail(token);
+            ResponseDTO responseDTO = ResponseDTO.builder().error("success").build();
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+}
---
+++ src/main/java/com/latteis/eumcoding/controller/UnauthMember.java
@@ -0,0 +1,126 @@
+package com.latteis.eumcoding.controller;
+
+import com.latteis.eumcoding.dto.MemberDTO;
+import com.latteis.eumcoding.dto.ResponseDTO;
+import com.latteis.eumcoding.security.TokenProvider;
+import com.latteis.eumcoding.service.EmailTokenService;
+import com.latteis.eumcoding.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/unauth/member")
+public class UnauthMember {
+
+    private final EmailTokenService emailTokenService;
+
+    private final TokenProvider tokenProvider;
+
+    private final MemberService memberService;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerMember(MemberDTO memberDTO) {
+
+        try {
+            MemberDTO registeredMember = memberService.add(memberDTO);
+            MemberDTO responseMemberDTO = MemberDTO.builder()
+                    .email(registeredMember.getEmail())
+                    .nickname(registeredMember.getNickname())
+                    .build();
+            //System.out.println(registeredMember.getId() + "아이디 번호");
+            //System.out.println(registeredMember + "DTO멤버");
+            emailTokenService.createEmailToken(registeredMember.getId(), registeredMember.getEmail()); // 이메일 전송
+            return ResponseEntity.ok().body(responseMemberDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+
+    }
+
+
+    // 로그인
+  @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody MemberDTO memberDTO) {
+      System.out.println("진입");
+        // 로그인 성공 시에만 MemberEntity 가져옴
+        MemberDTO successMemberDTO = memberService.getByCredentials(
+                memberDTO.getEmail(),
+                memberDTO.getPassword(),
+                passwordEncoder
+        );
+
+
+        // MemberEntity 가져오기 성공 시
+        if (successMemberDTO != null) {
+
+            // TokenProvider 클래스를 이용해 토큰을 생성한 후 MemberDTO에 넣어서 반환
+            final String token = tokenProvider.create(successMemberDTO);
+            MemberDTO responseMemberDTO = MemberDTO.builder()
+                    .email(successMemberDTO.getEmail())
+                    .id(successMemberDTO.getId())
+                    .token(token)
+                    .role(successMemberDTO.getRole())//멤버 타입
+                    .nickname(successMemberDTO.getNickname())
+                    .build();
+            return ResponseEntity.ok().body(responseMemberDTO);
+
+        } else {
+            // MemberEntity 가져오기 실패 시 -> 로그인 실패
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("login failed").build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+
+    }
+
+    @PostMapping("/sign")
+    public ResponseEntity<?> sign(@Param("email")String email, @Param("password") String password) {
+        System.out.println("진입22");
+        // 로그인 성공 시에만 MemberEntity 가져옴
+        MemberDTO successMemberDTO = memberService.getByCredentials(
+                email,
+                password,
+                passwordEncoder
+        );
+        System.out.println("진입중");
+        // MemberEntity 가져오기 성공 시
+        if (successMemberDTO != null) {
+            // TokenProvider 클래스를 이용해 토큰을 생성한 후 MemberDTO에 넣어서 반환
+            final String token = tokenProvider.create(successMemberDTO);
+            MemberDTO responseMemberDTO = MemberDTO.builder()
+                    .email(successMemberDTO.getEmail())
+                    .id(successMemberDTO.getId())
+                    .token(token)
+                    .role(successMemberDTO.getRole())//멤버 타입
+                    .nickname(successMemberDTO.getNickname())
+                    .build();
+            return ResponseEntity.ok().body(responseMemberDTO);
+        } else {
+            // MemberEntity 가져오기 실패 시 -> 로그인 실패
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("로그인 실패").build();
+            System.out.println(responseDTO + "실패햇어요");
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+
+    }
+
+
+
+
+
+
+}
