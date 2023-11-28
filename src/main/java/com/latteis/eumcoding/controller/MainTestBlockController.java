1d38a06b0d0572beaa7899e77675064121643a6b
daekyumyoo <daekyum.dev@gmail.com>
2023-11-28 11:15:21 +0900

    block convert

--- src/main/java/com/latteis/eumcoding/controller/MainTestBlockController.java
+++ src/main/java/com/latteis/eumcoding/controller/MainTestBlockController.java
@@ -1,10 +1,24 @@
 package com.latteis.eumcoding.controller;
 
+import com.latteis.eumcoding.dto.LectureDTO;
+import com.latteis.eumcoding.exception.ErrorCode;
+import com.latteis.eumcoding.exception.ResponseMessageException;
+import com.latteis.eumcoding.service.MainTestBlockService;
+import com.latteis.eumcoding.util.blockCoding.Block;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -12,4 +26,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lecture/section/test/question/block")
 @Api(tags = "Main Test Block Controller", description = "메인 테스트 블록코딩 컨트롤러")
 public class MainTestBlockController {
+
+    private final MainTestBlockService mainTestBlockService;
+
+    /*
+     * 블록 리스트 변환
+     */
+    @PostMapping(value = "/block-convert")
+    @ApiOperation(value = "블록 리스트 변환")
+    public ResponseEntity<String> convertBlocks(@ApiIgnore Authentication authentication, @Valid @RequestBody List<Block> blockList) {
+
+        try {
+            String blocks = mainTestBlockService.convertBlocks(authentication, blockList);
+            return ResponseEntity.ok().body(blocks);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ResponseMessageException(ErrorCode.INVALID_PARAMETER);
+        }
+
+    }
 }
--- src/main/java/com/latteis/eumcoding/service/MainTestBlockService.java
+++ src/main/java/com/latteis/eumcoding/service/MainTestBlockService.java
@@ -1,11 +1,56 @@
 package com.latteis.eumcoding.service;
 
+import com.latteis.eumcoding.exception.ErrorCode;
+import com.latteis.eumcoding.exception.ResponseMessageException;
+import com.latteis.eumcoding.model.MainTestAnswer;
+import com.latteis.eumcoding.util.blockCoding.Block;
+import com.latteis.eumcoding.util.blockCoding.BlockCodeToJavaConverter;
+import com.latteis.eumcoding.util.blockCoding.JavaCodeExecutor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MainTestBlockService {
+
+    private final BlockCodeToJavaConverter blockCodeToJavaConverter;
+
+    private final JavaCodeExecutor javaCodeExecutor = new JavaCodeExecutor();
+
+
+    /**
+     * @param authentication 로그인 정보
+     * @param blockList 블록 리스트
+     * @return string
+     */
+    public String convertBlocks(Authentication authentication, List<Block> blockList) {
+
+        if (blockList.isEmpty()) {
+            throw new ResponseMessageException(ErrorCode.INVALID_PARAMETER);
+        }
+
+        // 학생 답안을 자바 코드로 변환
+        String testString = blockCodeToJavaConverter.convertToJavaCode(blockList);
+
+        // 변환된 답안을 컴파일
+        String testAnswerString = null;
+
+        try{
+            testAnswerString = javaCodeExecutor.executeJavaCode(testString);
+        }catch(Exception e){
+            e.printStackTrace();
+            log.info("컴파일 에러");
+        }
+
+        // 문자열의 공백을 제거하고 비교
+        String formattedTestAnswer = testAnswerString.toString().trim();
+
+        return formattedTestAnswer;
+
+    }
 }
